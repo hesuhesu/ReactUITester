53627cf05171f3da65c95d39bec6f11f8a3054ff
hesuhesu <zndwk0728@gmail.com>
2024-10-25 16:28:21 +0900

    Modify scss

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 import '../css/Footer.scss'
 
 const Footer = () => {
     return (
         <footer className="footer">
-            <p>© 2024 나의 사이트. 모든 권리 보유.</p>
             <a href="https://github.com/hesuhesu" target="_blank" rel="noopener noreferrer" className="github-link">
                 <FontAwesomeIcon icon={faGithub} size="2x" />
             </a>
+            <a href={`mailto:${'hesuhesu@naver.com'}`} className="email-link">
+                <FontAwesomeIcon icon={faEnvelope} size="2x" />
+            </a>
+            <p>© 2024. hesuhesu. All rights reserved</p>
         </footer>
     );
 };
