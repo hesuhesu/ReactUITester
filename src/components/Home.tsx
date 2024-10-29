50c3226fed8cb2c4e246a966eb21767fea680c68
hesuhesu <zndwk0728@gmail.com>
2024-10-29 15:13:41 +0900

    Modify scss

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,8 +6,11 @@ const Home: React.FC = () => {
     return (
         <div className="home">
             <h2>Future Possibility</h2>
-            <strong>성장, 노력, 끈기</strong>
-            <p>Full Stack 개발자 은희수입니다!</p>
+            <div className="home-1">
+                <strong>성장, 노력, 끈기</strong>
+                <p>Full Stack 개발자 은희수입니다!</p>
+            </div>
+            
         </div>
     )
 }
